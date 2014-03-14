6bf699c8af6940c4c079a32e27cc6bcf1e89b802
tao12345666333 <tao12345666333@163.com>
2014-03-14 23:20:21 +0800

    update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+    
+  
   def show
   	@user = User.find(params[:id])
   end
@@ -11,10 +19,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Rails App!"
+      flash[:success] = "Welcome to the Rails_App!"
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit 
+    #@user = User.find(params[:id])  
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -23,5 +44,19 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
