c56a66942bbb14f1ad59a07448ddcd8ecb3da459
tao12345666333 <tao12345666333@163.com>
2014-03-23 16:20:10 +0800

    Finish user edit, index , update and destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only:[:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
   	@user = User.find(params[:id])
   end
@@ -9,11 +16,17 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = " Profile updated "
       redirect_to @user
@@ -41,7 +54,20 @@ class UsersController < ApplicationController
     end
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-        
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
       end
-end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end 
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?        
+      end
+end  
+
+    
+
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,9 +11,13 @@ module SessionsHelper
   	end
 
 	def current_user=(user)
-		@current_user = user
+		remember_token = User.encrypt(cookies[:remember_token])
+		@current_user ||= User.find_by(remember_token: remember_token)
 	end
 
+	def  current_user?(user)
+		user == current_user
+	end
 	def current_user
 		remember_token = User.encrypt(cookies[:remember_token])
 		@current_user ||= User.find_by(remember_token: remember_token)
@@ -25,4 +29,12 @@ module SessionsHelper
 		cookies.delete(:remember_token)
 	end
 
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+    		session.delete(:return_to)		
+	end
+
+	def  store_location
+		session[:return_to] = request.fullpath if request.get?		
+	end
 end
